8bd3d284d0a3a7c4c4192ad8109b561fd19a6113
seveneditor <sabori780@gmail.com>
2017-07-24 22:26:21 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "Block",
     "InGroups",
     },
-    sudo_users = {56693692,},--Sudo users
+    sudo_users = {212281884},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 ]],
